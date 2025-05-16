ebf1cf1abbc835ae7111e33215d79d6f3e557a3e
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-05-16 14:04:38 +0700

    initial init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import NuqsProvider from "@/providers/NuqsProvider";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReactQueryProvider>
+          <NuqsProvider>{children}</NuqsProvider>
+        </ReactQueryProvider>
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
